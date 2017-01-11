2094024d1c4b9c33cf8557c9ad162db2bf2bd4d1
Thang Phan <lyern52@gmail.com>
2017-01-11 19:56:43 +0100

    Change name folder

---
+++ app/src/main/java/thesiscsc/thesiscsc/splashscreen/SplashScreen.java
@@ -0,0 +1,32 @@
+package thesiscsc.thesiscsc.splashscreen;
+
+/**
+ * Created by tphan46 on 10.01.2017.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import thesiscsc.thesiscsc.MainActivity;
+
+public class SplashScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
