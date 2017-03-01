7d999cbf3c56eee696dedf2099b4abf1c0abcf34
wilson <andreas.wil@hotmail.com>
2017-03-01 14:19:29 +0100

    Home screen buttons added. Design not finished.

--- app/src/main/java/thesiscsc/thesiscsc/fragment/TestOneFragment.java
+++ app/src/main/java/thesiscsc/thesiscsc/fragment/TestOneFragment.java
@@ -1,14 +1,17 @@
 package thesiscsc.thesiscsc.fragment;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import thesiscsc.thesiscsc.R;
 
@@ -18,13 +21,23 @@ import thesiscsc.thesiscsc.R;
 public class TestOneFragment extends Fragment {
     public SharedPreferences prefs;
     private ListView lv;
+    private TextView text;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.testone_view, container, false);
-        lv = (ListView)view.findViewById(R.id.lv_channel);
+        //lv = (ListView)view.findViewById(R.id.lv_channel);
+        text = (TextView) view.findViewById(R.id.textView2);
+        CardView cardViewTask = (CardView) view.findViewById(R.id.viewTask);
+
+        cardViewTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Moving to Task Management");
+            }
+        });
 
         return view;
 
